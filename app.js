66f772b29b748a9c579628b768a9126abe757849
Dinuka De Silva <l.dinukadesilva@gmail.com>
2021-10-06 00:27:56 -0400

    Enabling the cors headers

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const fs = require('fs')
 const rfs = require('rotating-file-stream')
 const authenticator = require('./middleware/authenticator')
 
+const cors = require('cors')
+
 const indexRouter = require('./routes/index');
 const fileRouter = require('./routes/fileserver');
 
@@ -18,23 +20,30 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 
+app.use(cors({
+    exposedHeaders: ['Content-Type', 'Content-Length', "content-disposition"]
+}))
+
 app.use(authenticator)
 
 // log only 4xx and 5xx responses to console
 app.use(logger('dev', {
-  skip: function (req, res) { return res.statusCode < 400 }
+    skip: function (req, res) {
+        return res.statusCode < 400
+    }
 }))
 
 // create a rotating write stream
 const accessLogStream = rfs.createStream('access.log', {
-  interval: '1d', // rotate daily
-  path: path.join(__dirname, 'log')
+    interval: '1d', // rotate daily
+    path: path.join(__dirname, 'log')
 })
 // log all requests to access.log
 app.use(logger('combined', {
-  stream: accessLogStream}))
+    stream: accessLogStream
+}))
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(authenticator)
@@ -43,19 +52,19 @@ app.use('/', indexRouter);
 app.use('/files', fileRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
