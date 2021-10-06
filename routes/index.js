a7171b7d3aed7767100d8fe13b783a3e1cad3d6e
Dinuka De Silva <l.dinukadesilva@gmail.com>
2021-10-06 00:27:15 -0400

    gh-4: Adding the `/curl-script` endpoint

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+router.get('/curl-script', function (req, res, next) {
+    res.set({"content-disposition": "attachment; filename=\"emc-curl-script.sh\""});
+    res.send(
+        `
+# TODO
+        `
+    );
 });
 
 module.exports = router;
